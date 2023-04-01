f31f0ad953a726f30cb49ba545b889a162386567
Ali Akbarzadeh <akbarzadeh.ali77@gmail.com>
2023-04-01 17:19:26 +0430

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,tsx,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        fraunces: "'Fraunces 144pt S000', sans-serif",
+        manrope: "'Manrope', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
